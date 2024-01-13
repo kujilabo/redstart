faebce714f5554dbce5b00b947123e39641bbf2c
pecolynx <pecolynx@gmail.com>
2024-01-13 13:31:11 +0900

    develop

--- lib/config/config_db.go
+++ lib/config/config_db.go
@@ -25,61 +25,92 @@ type MySQLConfig struct {
 	Database string `yaml:"database" validate:"required"`
 }
 
-type DBConfig struct {
-	DriverName string         `yaml:"driverName"`
-	SQLite3    *SQLite3Config `yaml:"sqlite3"`
-	MySQL      *MySQLConfig   `yaml:"mysql"`
-	Migration  bool           `yaml:"migration"`
+type PostgresConfig struct {
+	Username string `yaml:"username" validate:"required"`
+	Password string `yaml:"password" validate:"required"`
+	Host     string `yaml:"host" validate:"required"`
+	Port     int    `yaml:"port" validate:"required"`
+	Database string `yaml:"database" validate:"required"`
 }
 
-func InitDB(cfg *DBConfig, sqlFS embed.FS) (*gorm.DB, *sql.DB, error) {
+type DBConfig struct {
+	DriverName string          `yaml:"driverName"`
+	SQLite3    *SQLite3Config  `yaml:"sqlite3"`
+	MySQL      *MySQLConfig    `yaml:"mysql"`
+	Postgres   *PostgresConfig `yaml:"postgres"`
+	Migration  bool            `yaml:"migration"`
+}
+
+func InitDB(cfg *DBConfig, sqlFS embed.FS) (libgateway.DialectRDBMS, *gorm.DB, *sql.DB, error) {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	}))
 	switch cfg.DriverName {
-	case "sqlite3":
-		db, err := libgateway.OpenSQLite("./"+cfg.SQLite3.File, logger)
-		if err != nil {
-			return nil, nil, liberrors.Errorf("OpenSQLite. err: %w", err)
-		}
+	// case "sqlite3":
+	// 	db, err := libgateway.OpenSQLite("./"+cfg.SQLite3.File, logger)
+	// 	if err != nil {
+	// 		return nil, nil, liberrors.Errorf("OpenSQLite. err: %w", err)
+	// 	}
 
-		sqlDB, err := db.DB()
-		if err != nil {
-			return nil, nil, err
-		}
+	// 	sqlDB, err := db.DB()
+	// 	if err != nil {
+	// 		return nil, nil, err
+	// 	}
 
-		if err := sqlDB.Ping(); err != nil {
-			return nil, nil, err
-		}
+	// 	if err := sqlDB.Ping(); err != nil {
+	// 		return nil, nil, err
+	// 	}
 
-		if cfg.Migration {
-			if err := libgateway.MigrateSQLiteDB(db, sqlFS); err != nil {
-				return nil, nil, err
-			}
-		}
+	// 	if cfg.Migration {
+	// 		if err := libgateway.MigrateSQLiteDB(db, sqlFS); err != nil {
+	// 			return nil, nil, err
+	// 		}
+	// 	}
 
-		return db, sqlDB, nil
+	// 	return db, sqlDB, nil
 	case "mysql":
 		db, err := libgateway.OpenMySQL(cfg.MySQL.Username, cfg.MySQL.Password, cfg.MySQL.Host, cfg.MySQL.Port, cfg.MySQL.Database, logger)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, nil, err
 		}
 
 		sqlDB, err := db.DB()
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, nil, err
 		}
 
 		if err := sqlDB.Ping(); err != nil {
-			return nil, nil, err
+			return nil, nil, nil, err
 		}
 
 		if err := libgateway.MigrateMySQLDB(db, sqlFS); err != nil {
-			return nil, nil, liberrors.Errorf("failed to MigrateMySQLDB. err: %w", err)
+			return nil, nil, nil, liberrors.Errorf("failed to MigrateMySQLDB. err: %w", err)
 		}
 
-		return db, sqlDB, nil
+		dialect := libgateway.DialectMySQL{}
+		return &dialect, db, sqlDB, nil
+	case "postgres":
+		db, err := libgateway.OpenPostgres(cfg.Postgres.Username, cfg.Postgres.Password, cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.Database, logger)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		sqlDB, err := db.DB()
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		if err := sqlDB.Ping(); err != nil {
+			return nil, nil, nil, err
+		}
+
+		if err := libgateway.MigratePostgresDB(db, sqlFS); err != nil {
+			return nil, nil, nil, liberrors.Errorf("failed to MigrateMySQLDB. err: %w", err)
+		}
+
+		dialect := libgateway.DialectPostgres{}
+		return &dialect, db, sqlDB, nil
 	default:
-		return nil, nil, libdomain.ErrInvalidArgument
+		return nil, nil, nil, libdomain.ErrInvalidArgument
 	}
 }
